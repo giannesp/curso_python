414ddd2660beedcc9a100b88f22179ac15177260
Giannesp <gianne814@gmail.com>
2020-08-11 09:43:46 -0300

    Aula 01

---
+++ 01.py
@@ -0,0 +1,10 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nome = 'Gianne'
+>>> idade = 25
+>>> peso = 58
+>>> print('nome,idade, peso')
+nome,idade, peso
+>>> print(nome, idade, peso)
+Gianne 25 58
+>>> 
---
+++ ex003.py
@@ -0,0 +1,4 @@
+número1 = int(input('Primeiro número: '))
+número2 = int(input('Segundo número: '))
+soma = número1+número2
+input('Soma equivale á {}!'.format(soma))
